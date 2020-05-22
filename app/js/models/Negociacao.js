8766c20a68aaade001cabf238e030aeaeea87a6d
Wellington Correia Martins <wmartins@server01.foursys.local>
2020-05-22 16:59:53 -0300

    Aula 22/05/2020

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,20 @@
+class Negociacao {
+    // declaração das propriedades da classe
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
+    }
+    get data() {
+        return this._data;
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+}
